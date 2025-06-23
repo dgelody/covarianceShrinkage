63f17e1160b12fc37e56b1541ff1952cbbb89292
David Gelody <david.gelody@frgrisk.com>
2025-06-23 15:37:47 -0400

    Updated argument and return types and updated test case

--- linearShrinkage.go
+++ linearShrinkage.go
@@ -11,51 +11,66 @@ import (
 	"github.com/gonum/floats"
 	"gonum.org/v1/gonum/blas"
 	"gonum.org/v1/gonum/blas/blas64"
+	"gonum.org/v1/gonum/mat"
 )
 
-// Returns a covariance estimate using linear shrinkage.
-// The target preserves the diagonal of the sample covariance matrix and
-// all correlation coefficients are the same.
-//
-// Arguments:
-// n int: number of assets
-// t int: number of periods
-// X []float64: matrix of n assets by t periods
-// S []float63: nxn covariance matrix
-//
-// Returns:
-// []float64: nxn shrinkage estimator of the covariance matrix
-// float64: Frobenius norm between covariance matrix and shrinkage matrix
-// error
-func CovCor(n int, t int, X []float64, S []float64) (F []float64, norm float64, err error) {
+type Target int
+
+const (
+	cov1Para Target = iota
+	cov2Para
+	covCor
+	covDiag
+	covMarket
+)
+
+func linShrink(X *mat.Dense, S *mat.SymDense, ttype Target) (F []float64, norm float64, err error) {
+
+	rawX := X.RawMatrix().Data
+
+	n := X.RawMatrix().Rows
+	t := X.RawMatrix().Cols
+
 	// Sanity Checks
-	if t <= 0 || n <= 0 {
-		return nil, 0.0, errors.New("t and n must be > 0")
+	if t <= 1 || n <= 0 {
+		return nil, 0.0, errors.New("n < 0 or t < 2")
 	}
-	if len(X) != t*n {
-		return nil, 0.0, errors.New("len(X) != t * n")
-	}
-	if S != nil && len(S) != n*n {
-		return nil, 0.0, errors.New("len(S) != n * n")
-	}
+	// if len(X) != t*n {
+	// 	return nil, 0.0, errors.New("len(X) != t * n")
+	// }
+	// if S != nil && len(S) != n*n {
+	// 	return nil, 0.0, errors.New("len(S) != n * n")
+	// }
+	// if X == nil {
+	// 	return nil, 0.0, errors.New("matrix X is nil")
+	// }
 
-	if X == nil {
-		return nil, 0.0, errors.New("matrix X is nil")
-	}
-
+	// If covariance matrix is not given, use sample covariance matrix
 	var covMatrix []float64
 	if S == nil {
-		covMatrix = SampleCovarianceMatrix(n, t, X)
+		covMatrix = SampleCovarianceMatrix(n, t, rawX)
 	} else {
-		covMatrix = S
+		covMatrix = S.RawSymmetric().Data
+	}
+
+	switch ttype {
+	case cov1Para:
+		F = cov1ParaTarget(n, covMatrix)
+	case cov2Para:
+		F = cov2ParaTarget(n, covMatrix)
+	case covCor:
+		F = covCorTarget(n, covMatrix)
+	case covDiag:
+		F = covDiagTarget(n, covMatrix)
+	case covMarket:
+		F = covMarketTarget(n, covMatrix)
 	}
 
 	//To measure Frobenius norm afterwards
 	originalCovMatrix := make([]float64, len(covMatrix))
 	copy(originalCovMatrix, covMatrix)
 
-	F = shrinkageTarget(n, covMatrix)
-	delta := shrinkageIntensity(n, t, X, covMatrix)
+	delta := shrinkageIntensity(n, t, rawX, covMatrix)
 
 	// shrinkage estimator of the covariance matrix = delta*F + (1-delta)*S
 	floats.Scale(delta, F)
@@ -67,11 +82,73 @@ func CovCor(n int, t int, X []float64, S []float64) (F []float64, norm float64, 
 	return F, froNorm, nil
 }
 
+// Returns a covariance estimate using linear shrinkage.
+// The target preserves the diagonal of the sample covariance matrix and
+// all correlation coefficients are the same.
+//
+// Arguments:
+// X *mat.Dense : matrix of n by t observations. The data used to create the covariance matrix.
+//
+//	Should be differenced if the covariance matrix is the covariance of the differences.
+//
+// S *mat.SymDense: nxn covariance matrix. If nil, S will be the sample covariance matrix
+//
+// Returns:
+// F *mat.SymDense: nxn shrinkage estimator of the covariance matrix
+// norm float64: Frobenius norm between covariance matrix and shrinkage matrix
+// err error
+func CovCor(X *mat.Dense, S *mat.SymDense) (FMatrix *mat.SymDense, norm float64, err error) {
+
+	rawX := X.RawMatrix().Data
+
+	n := X.RawMatrix().Rows
+	t := X.RawMatrix().Cols
+	// Sanity Checks
+	if t <= 1 || n <= 0 {
+		return nil, 0.0, errors.New("t and n must be > 0")
+	}
+	// if len(X) != t*n {
+	// 	return nil, 0.0, errors.New("len(X) != t * n")
+	// }
+	// if S != nil && len(S) != n*n {
+	// 	return nil, 0.0, errors.New("len(S) != n * n")
+	// }
+
+	// if X == nil {
+	// 	return nil, 0.0, errors.New("matrix X is nil")
+	// }
+
+	var covMatrix []float64
+	if S == nil {
+		covMatrix = SampleCovarianceMatrix(n, t, rawX)
+	} else {
+		covMatrix = S.RawSymmetric().Data
+	}
+
+	//To measure Frobenius norm afterwards
+	originalCovMatrix := make([]float64, len(covMatrix))
+	copy(originalCovMatrix, covMatrix)
+
+	F := covCorTarget(n, covMatrix)
+	delta := shrinkageIntensity(n, t, rawX, covMatrix)
+
+	// shrinkage estimator of the covariance matrix = delta*F + (1-delta)*S
+	floats.Scale(delta, F)
+	floats.Scale(1.0-delta, covMatrix)
+	floats.Add(F, covMatrix)
+
+	froNorm := FrobeniusNorm(originalCovMatrix, F)
+
+	FMatrix = mat.NewSymDense(n, F)
+
+	return FMatrix, froNorm, nil
+}
+
 // Computes the sample covariance matrix
 //
 // Arguments:
 // n int: number of assets
-// T int: number of periods
+// t int: number of periods
 // X []float64: matrix of n assets by t periods
 //
 // Returns:
@@ -145,7 +222,7 @@ func FrobeniusNorm(A, B []float64) float64 {
 //
 // Returns:
 // F []float64: nxn shrinkage target
-func shrinkageTarget(n int, s []float64) (F []float64) {
+func covCorTarget(n int, s []float64) (F []float64) {
 	r_bar := avgSampleCorr(n, s)
 
 	F = make([]float64, n*n)
@@ -158,6 +235,39 @@ func shrinkageTarget(n int, s []float64) (F []float64) {
 			}
 		}
 	}
+
+	return F
+}
+
+func cov1ParaTarget(n int, s []float64) (F []float64) {
+	sum := 0.0
+	for i := 0; i < n; i++ {
+		sum += s[i*n+i]
+	}
+	diagMean := sum / float64(n)
+
+	F = make([]float64, n*n)
+	for i := 0; i < n; i++ {
+		F[i*n+i] = diagMean
+	}
+
+	return F
+}
+
+func cov2ParaTarget(n int, s []float64) (F []float64) {
+	// TODO
+
+	return F
+}
+
+func covDiagTarget(n int, s []float64) (F []float64) {
+	// TODO
+
+	return F
+}
+
+func covMarketTarget(n int, s []float64) (F []float64) {
+	// TODO
 
 	return F
 }
@@ -235,7 +345,7 @@ func shrinkageIntensity(n int, T int, X []float64, s []float64) (delta float64) 
 
 	// Estimator for gamma
 	gamma_hat := 0.0
-	F := shrinkageTarget(n, s)
+	F := covCorTarget(n, s)
 	for i := 0; i < n*n; i++ {
 		diff := F[i] - s[i]
 		gamma_hat += diff * diff
--- linearShrinkage_test.go
+++ linearShrinkage_test.go
@@ -5,97 +5,102 @@ package covarianceShrinkage
 import (
 	"math"
 	"testing"
+
+	"gonum.org/v1/gonum/mat"
 )
 
 func TestCovarianceShrinkage(t *testing.T) {
 	type args struct {
-		n int
-		t int
-		X []float64
-		S []float64
+		X *mat.Dense
+		S *mat.SymDense
 	}
 	tests := []struct {
 		name    string
 		args    args
-		want    []float64
+		want    *mat.SymDense
 		want1   float64
 		wantErr bool
 	}{
 		{
 			name: "mixed returns #1 (n=4,t=3)",
 			args: args{
-				n: 4,
-				t: 3,
-				X: []float64{
+				X: mat.NewDense(4, 3, []float64{
 					4, 1, 3,
 					2, 5, 4,
 					3, 2, 1,
 					1, 6, 7,
-				},
+				}),
+				S: nil,
 			},
-			want: []float64{
+			want: mat.NewSymDense(4, []float64{
 				2.333333, -2.035832, 0.407353, -3.020793,
 				-2.035832, 2.333333, -0.953236, 3.782154,
 				0.407353, -0.953236, 1.000000, -2.818354,
 				-3.020793, 3.782154, -2.818354, 10.333333,
-			},
+			}),
 			want1: 0.8785905137256039,
 		},
 		{
 			name: "mixed returns #2 (n=4,t=3)",
 			args: args{
-				n: 4,
-				t: 3,
-				X: []float64{
+				X: mat.NewDense(4, 3, []float64{
 					1, 2, 3,
 					2, 4, 1,
 					3, 6, 4,
 					4, 8, 2,
-				},
+				}),
+				S: nil,
 			},
-			want: []float64{
+			want: mat.NewSymDense(4, []float64{
 				1.000000, -0.283402, 0.514419, -0.566805,
 				-0.283402, 2.333333, 1.639115, 4.076053,
 				0.514419, 1.639115, 2.333333, 3.278231,
 				-0.566805, 4.076053, 3.278231, 9.333333,
-			},
+			}),
 			want1: 1.2427458025566596,
 		},
 		{
 			name: "perfect multiples (n=5,t=3)",
 			args: args{
-				n: 5,
-				t: 3,
-				X: []float64{
+				X: mat.NewDense(5, 3, []float64{
 					1, 2, 3,
 					2, 4, 6,
 					3, 6, 9,
 					4, 8, 12,
 					5, 10, 15,
-				},
+				}),
+				S: nil,
 			},
-			want: []float64{
+			want: mat.NewSymDense(5, []float64{
 				1, 2, 3, 4, 5,
 				2, 4, 6, 8, 10,
 				3, 6, 9, 12, 15,
 				4, 8, 12, 16, 20,
 				5, 10, 15, 20, 25,
-			},
+			}),
 			want1: 0.0,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, got1, err := CovCor(tt.args.n, tt.args.t, tt.args.X, tt.args.S)
+			got, got1, err := CovCor(tt.args.X, tt.args.S)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("CovarianceShrinkage() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			for i := range got {
-				if math.Abs(got[i]-tt.want[i]) > 1e-6 {
-					t.Errorf("CovarianceShrinkage() element %d: got %v, want %v", i, got[i], tt.want[i])
+
+			// Compare got with want
+			if got.SymmetricDim() != tt.want.SymmetricDim() {
+				t.Fatalf("Dimension mismatch: got %d, want %d", got.SymmetricDim(), tt.want.SymmetricDim())
+			}
+			for i := 0; i < got.SymmetricDim(); i++ {
+				for j := 0; j <= i; j++ {
+					if math.Abs(got.At(i, j)-tt.want.At(i, j)) > 1e-6 {
+						t.Errorf("CovarianceShrinkage()[%d,%d] = %v, want %v", i, j, got.At(i, j), tt.want.At(i, j))
+					}
 				}
 			}
+
 			if math.Abs(got1-tt.want1) > 1e-6 {
 				t.Errorf("CovarianceShrinkage() got1 = %v, want %v", got1, tt.want1)
 			}
